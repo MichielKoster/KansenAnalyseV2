01d2e2b258f95a2eeb7440597fbd4401ef78c8fe
MichielKoster <michielk@hotmail.com>
2026-06-16 11:18:53 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html')
+    },
     chunkSizeWarningLimit: 2000,
-  },
-  server: {
-    port: 3000,
-    open: true,
   }
 })
